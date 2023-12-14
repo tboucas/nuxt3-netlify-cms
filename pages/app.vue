8c0255186fa20d07ea402ce0cc702c5d54059511
tiagoboucas <tiagoboucas@hotmail.com>
2023-12-14 08:21:01 +0000

    remove old script

--- pages/app.vue
+++ pages/app.vue
@@ -6,15 +6,4 @@
       </ul>
     </nav>
   </header>
-  <script>
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", (user) => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-  </script>
 </template>
